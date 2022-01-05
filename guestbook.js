3b408e39cf984ccef1a1e72592bb9d9ec730fd95
alagarajaR <alagarajaR@gmail.com>
2022-01-05 22:58:59 +0530

    Bug Fixed with regards to API URL

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://github.com/alagarajaR/serverless-guestbook.git';
+const apiUrl = 'https://af2b7b0c.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
