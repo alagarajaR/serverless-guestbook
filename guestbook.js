0cda51cf83ddc673f7ce3d21efb0a9149a316e2f
alagarajaR <alagarajaR@gmail.com>
2022-01-05 22:49:16 +0530

    API URL Chnaged

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://github.com/alagarajaR/serverless-guestbook.git';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
